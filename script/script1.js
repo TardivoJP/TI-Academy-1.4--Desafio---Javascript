4f1ecb620b7ea0788b8ff6c6de7f990553bcf0dd
TardivoJP <tardivojoaopedro@gmail.com>
2022-05-30 02:02:14 -0300

    Melhoras ao código

--- script/script1.js
+++ script/script1.js
@@ -17,19 +17,27 @@ window.onload=function(){
     ]
 
     verificarDados.addEventListener("click",function(){
+        //criamos essa variável para determinar se o usuário
+        //existe ou não para depois verificarmos a senha
         let usuExist=0;
 
+        //percorremos a array para verificar os usuarios
         for(let arr of listaUsuarios){
             for(let key in arr){
 
                 if(key=="user"&&usuario.value==arr[key]){
+                    //se entrarmos neste if, encontramos um usuário válido
+                    //agora vamos verificar se a senha bate com a dele no sistema
                     usuExist=1;
 
                     if(senha.value==arr["pws"]){
-                        //alert("Usuário e senha corretos!");
+                        //se ambos o usuario existe e a senha deste for correta
+                        //então encaminhamos o usário para a próxima página
                         location.href='pagina2.html';
                     }else{
                         if(senha.value!=arr["pws"]){
+                            //caso a senha para este usuário for incorreta
+                            //enviamos um alerta na tela do usuário
                             alert("Senha errada!");
                         }
                     }
@@ -37,6 +45,8 @@ window.onload=function(){
             }
         }
 
+        //caso um usuário inválido tenha sido digitado
+        //enviamos um alerta na tela
         if(usuExist!=1){
             alert("Usuário não existe!");
         }
--- script/script2.js
+++ script/script2.js
@@ -19,9 +19,11 @@ window.onload=function(){
         },         
     ]
     
+    //variável para a data atual
     let dataAtual=new Date();
     let dataAtualFormatada=dataAtual.toLocaleString("pt-BR",{year:"numeric",month:"2-digit",day:"2-digit"});
 
+    //elementos HTML que vão ser manipulados
     const btnAbrirClientes=document.querySelector("#btnAbrirClientes");
     const janelaClientes=document.querySelector("#clientes");
     
@@ -37,17 +39,18 @@ window.onload=function(){
 
     const btnFecharCliente=document.querySelector("#btn-fechar-cliente");
 
+    //variável para servir como chava para a array da lista dos clientes
     let seletorCliente=0;
 
+    //função para pegar os dados de cada código e mostrar na tela para o usuário 
     function atualizarCliente(arrKey){
             codCliente.value=listaDosClientes[arrKey]["codCliente"];
             nomeCliente.value=listaDosClientes[arrKey]["nomeCliente"];
             dataCadCliente.value=listaDosClientes[arrKey]["dataCadCli"];
     }
 
-    atualizarCliente(seletorCliente);
-
-
+    //evento para a seta direita da interface
+    //caso chegarmos em um valor fora da array enviamos um alerta ao usuário
     avancarCliente.addEventListener("click",function(){
         if(seletorCliente<listaDosClientes.length-1){
             seletorCliente++;
@@ -57,6 +60,7 @@ window.onload=function(){
         }  
     })
 
+    //evento bem similar para a seta esquerda da interface
     voltarCliente.addEventListener("click",function(){
         if(seletorCliente>0){
             seletorCliente--;
@@ -66,12 +70,23 @@ window.onload=function(){
         }
     })
 
+    //evento para o botao de novo cliente
+    //inserimos o próximo código
+    //limpamos o nome para o usuario inserir o que desejar
+    //inserimos a data atual conforme declarada anteriormente
     novoCliente.addEventListener("click",function(){       
         codCliente.value=listaDosClientes.length+1;
         nomeCliente.value="";
         dataCadCliente.value=dataAtualFormatada;
     })
 
+    //evento para salvar um cliente criado
+    //caso este botao for clicado antes de clicar no botao novo
+    //o usuario vai receber um alerta
+    //
+    //se o usuario utilizar corretamente o evento vai
+    //realisar um push() para a array com as novas informações
+    //além disso ele volta para o código 1 conforme os parâmetros do desafio
     salvarCliente.addEventListener("click",function(){
 
         if( !(codCliente.value>=0&&codCliente.value<=listaDosClientes.length) ){
@@ -94,16 +109,23 @@ window.onload=function(){
         }
     })
 
+    //evento para fechar a janela dos clientes
     btnFecharCliente.addEventListener("click",function(){
         janelaClientes.classList.add("display-none");
     })
 
+    //evento para abrir a janela dos clientes
+    //certificamos de que todos as outras janelas estão fechadas
+    //bem como chamamos a nossa função para mostrar
+    //os clientes começando pelo código 1
+    //conforme os parâmetros do desafio
     btnAbrirClientes.addEventListener("click",function(){
         janelaProdutos.classList.add("display-none");
+        janelaPedidos.classList.add("display-none");
         janelaClientes.classList.remove("display-none");
         atualizarCliente(seletorCliente);
     })
-    //fimd dos clientes
+    //fim dos clientes
 
 
 
@@ -125,6 +147,7 @@ window.onload=function(){
     ]
 
 
+    //elementos HTML que vão ser manipulados
     const btnAbrirProdutos=document.querySelector("#btnAbrirProdutos");
     const janelaProdutos=document.querySelector("#produtos");
     
@@ -141,8 +164,10 @@ window.onload=function(){
 
     const btnFecharProduto=document.querySelector("#btn-fechar-produto");
 
+    //variável para servir como chava para a array da lista dos produtos
     let seletorProduto=0;
 
+    //função para pegar os dados de cada código e mostrar na tela para o usuário
     function atualizarProduto(arrKey){
             codProduto.value=listaDosProdutos[arrKey]["codProduto"];
             descricaoProduto.value=listaDosProdutos[arrKey]["descProduto"];
@@ -150,8 +175,8 @@ window.onload=function(){
             quantidadeProduto.value=listaDosProdutos[arrKey]["qtdEstoqueProd"];
     }
 
-    atualizarProduto(seletorProduto);
-
+    //evento para a seta direita da interface
+    //similar ao realizado para os clientes
     avancarProduto.addEventListener("click",function(){
         if(seletorProduto<listaDosProdutos.length-1){
             seletorProduto++;
@@ -161,6 +186,8 @@ window.onload=function(){
         }  
     })
 
+    //evento para a seta esquerda da interface
+    //similar ao realizado para os clientes
     voltarProduto.addEventListener("click",function(){
         if(seletorProduto>0){
             seletorProduto--;
@@ -170,6 +197,8 @@ window.onload=function(){
         }
     })
 
+    //evento para o botao de novo produto
+    //similar ao realizado para os clientes
     novoProduto.addEventListener("click",function(){
         codProduto.value=listaDosProdutos.length+1;
         descricaoProduto.value="";
@@ -177,6 +206,11 @@ window.onload=function(){
         quantidadeProduto.value="";
     })
 
+    //evento para salvar um produto criado
+    //similar ao realizado para os clientes
+    //porem precisamos de um pouco mais de atenção
+    //quanto a alguns valores devido as difrenças
+    //entre string e number
     salvarProduto.addEventListener("click",function(){
 
         if( !(codProduto.value>=0&&codProduto.value<=listaDosProdutos.length) ){
@@ -206,12 +240,17 @@ window.onload=function(){
         }
     })
 
+    //evento para fechar a janela dos produtos
+    //similar ao realizado para os clientes
     btnFecharProduto.addEventListener("click",function(){
         janelaProdutos.classList.add("display-none");
     })
 
+    //evento para abrir a janela dos produtos
+    //similar ao realizado para os clientes
     btnAbrirProdutos.addEventListener("click",function(){
         janelaClientes.classList.add("display-none");
+        janelaPedidos.classList.add("display-none");
         janelaProdutos.classList.remove("display-none");
         atualizarProduto(seletorProduto);
     })
@@ -219,6 +258,12 @@ window.onload=function(){
 
 
 
+    //começo dos pedidos
+
+    //elementos HTML que vão ser manipulados
+    //optei por declarar novas contantes para cada um deles
+    //para não acontecer qualquer confusão ou alteração
+    //na visualização das caixas de informação dos clientes ou produtos
     const btnAbrirPedidos=document.querySelector("#btnAbrirPedidos");
     const janelaPedidos=document.querySelector("#pedidos");
     
@@ -230,29 +275,125 @@ window.onload=function(){
     const precoProdutoPedido=document.querySelector("#preco-produto-pedido");
     const quantidadeProdutoPedido=document.querySelector("#quantidade-produto-pedido");
 
+    const lancarPedido=document.querySelector("#lancar-pedido");
+
+    const pedidoItem=document.querySelector("#pedido-item");
+    const pedidoDescricao=document.querySelector("#pedido-descricao");
+    const pedidoPreco=document.querySelector("#pedido-preco");
+    const pedidoQtd=document.querySelector("#pedido-qtd");
+    const pedidoSubTotal=document.querySelector("#pedido-sub-total");
+
+    const containerValorTotalPedido=document.querySelector("#container-valor-total-pedido");
+    const valorTotalPedido=document.querySelector("#valor-total-pedido");
+
     const btnFecharPedido=document.querySelector("#btn-fechar-pedido");
 
 
+    //evento para pegarmos as informações do código digitado pelo usuário
+    //nota-se o metodo blur ao inves de click conforme os parametros do desafio
     codClientePedido.addEventListener("blur",function(){
         let arrayKey=Number(codClientePedido.value)-1;
             nomeClientePedido.value=listaDosClientes[arrayKey]["nomeCliente"];
     })
 
+    //evento similar para o produto, porem deixamos a quantidade à critério
+    //do usuário
     codProdutoPedido.addEventListener("blur",function(){
         let arrayKey=Number(codProdutoPedido.value)-1;
         descricaoProdutoPedido.value=listaDosProdutos[arrayKey]["descProduto"];
         precoProdutoPedido.value=listaDosProdutos[arrayKey]["precoProduto"];
-        quantidadeProdutoPedido.value=listaDosProdutos[arrayKey]["qtdEstoqueProd"];
     })
 
+    //aqui declaramos duas variáveis, uma para o total a ser calculado no final do pedido
+    //e a segunda para criar uma array com os items que já foram adicionados ao carrinho
+    //pois um dos parametros do desafio é evitar a repetição de items no carrinho
+    let total=0;
+    let itemNoCarrinho=[];
 
-    console.log(listaDosClientes);
-    console.log(listaDosClientes.length);
-    console.log(listaDosClientes[0]);
-    console.log(listaDosClientes[0]["codCliente"]);
-    console.log(listaDosClientes[0]["nomeCliente"]);
-    console.log(listaDosClientes[0]["dataCadCli"]);
+    //evento para lancar o pedido no carrinho
+    lancarPedido.addEventListener("click",function(){
 
+        //declaramos uma variavel local para buscarmos as informacoes do produto
+        let arrayKey=Number(codProdutoPedido.value)-1;
 
+        //verificamos se o usuario preencheu todos os campos
+        if(codProdutoPedido.value==""||quantidadeProdutoPedido.value==""){
+            alert("Você não inseriu todos os dados necessários para realizar o pedido!");
+        }else{
+            //verificamos se o usuario digitou valores numericos
+            if(isNaN(Number(codProdutoPedido.value))||isNaN(Number(quantidadeProdutoPedido.value))){
+                alert("Você não inseriu um código ou quantidade válidos!");
+            }else{
+                //verificamos se o usuario digitou um valor maior que o estoque
+                //do produto selecionado
+                if(Number(quantidadeProdutoPedido.value)>listaDosProdutos[arrayKey]["qtdEstoqueProd"]){
+                    alert("Estoque insuficiente para esta quantidade!");
+                }else{
+                    //verificamos se o usuario digitou um valor float para
+                    //a quantidade
+                    if((Number(quantidadeProdutoPedido.value % 1)!==0)){
+                        alert("Insira um valor inteiro para a quantidade!");
+                    }else{
+                        //verificamos se o usuario inseriu um item repetido no carrinho
+                        if(itemNoCarrinho.includes(codProdutoPedido.value)){
+                            alert("Este item já está cadastrado!");
+                        }else{
+                            //se tudo corre bem, chegamos neste ponto
+                            //aqui adicionamos o código do item inserido para a nossa
+                            //array declarada anteriormente para verificarmos
+                            //se o usuario posteriormente não vai adiconar items repetidos
+                            itemNoCarrinho.push(codProdutoPedido.value);
+
+                            //aqui poderiamos usar esta linha abaixo
+                            //para subtrair o estoque de cada produto
+                            //porem como o desafio não permite itens repetidos
+                            //no carrinho, deixei ela comentada
+                            //listaDosProdutos[arrayKey]["qtdEstoqueProd"]=(listaDosProdutos[arrayKey]["qtdEstoqueProd"])-(Number(quantidadeProdutoPedido.value));
+
+                            //finalmente adicionamos cada item no carrinho
+                            let divPedidoItem=document.createElement("div");
+                                pedidoItem.appendChild(divPedidoItem).textContent=codProdutoPedido.value;
+
+                            let divPedidoDescricao=document.createElement("div");
+                                pedidoDescricao.appendChild(divPedidoDescricao).textContent=descricaoProdutoPedido.value;
+
+                            let divPedidoPreco=document.createElement("div");
+                                pedidoPreco.appendChild(divPedidoPreco).textContent=precoProdutoPedido.value;
+
+                            let divPedidoQtd=document.createElement("div");
+                                pedidoQtd.appendChild(divPedidoQtd).textContent=quantidadeProdutoPedido.value;
+
+                            //aqui realizamos um simples calculo para verificar o subtotal deste item
+                            let divPedidoSubTotal=document.createElement("div");
+                                pedidoSubTotal.appendChild(divPedidoSubTotal).textContent=(precoProdutoPedido.value)*(quantidadeProdutoPedido.value);
+
+                            //adicionamos o nosso subtotal ao total geral do carrinho
+                            total=total+((precoProdutoPedido.value)*(quantidadeProdutoPedido.value));
+                            valorTotalPedido.textContent=total.toLocaleString(`pt-BR`,{style:`currency`,currency:`BRL`});
+                            
+                            //removemos o display none para mostrarmos o total geral ao usuario
+                            //conforme os parametros do desafio
+                            containerValorTotalPedido.classList.remove("display-none");
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    //evento para fechar a janela dos pedidos
+    //similar ao realizado para os clientes e produtos
+    btnFecharPedido.addEventListener("click",function(){
+        janelaPedidos.classList.add("display-none");
+    })
+
+    //evento para abrir a janela dos pedidos
+    //similar ao realizado para os clientes e produtos
+    btnAbrirPedidos.addEventListener("click",function(){
+        janelaClientes.classList.add("display-none");
+        janelaProdutos.classList.add("display-none");
+        janelaPedidos.classList.remove("display-none");
+    })
+    //fim dos pedidos
 
 }
